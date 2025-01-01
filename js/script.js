75cf5f23f43d9a1ddfdc088c1942973f9bc0eb1b
艾小仙 <415586662@qq.com>
2025-01-01 20:49:05 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成"];
+const randomWords = ["小月我爱你", "小月我想你","小月快乐","小月好棒","小月小月"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "60px");
